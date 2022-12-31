a6dc6ae8d9ddca7a6273fd855f068fae172ebe0c
sipkab <10866741+Sipkab@users.noreply.github.com>
2022-12-31 12:29:05 +0100

    Don't replace StringBuilder capacity constructor

--- tool/src/sipka/jvm/constexpr/tool/StringBuilderConstantDeconstructor.java
+++ tool/src/sipka/jvm/constexpr/tool/StringBuilderConstantDeconstructor.java
@@ -4,13 +4,17 @@ import sipka.jvm.constexpr.tool.options.DeconstructionDataAccessor;
 import sipka.jvm.constexpr.tool.thirdparty.org.objectweb.asm.tree.MethodNode;
 
 final class StringBuilderConstantDeconstructor implements ConstantDeconstructor {
+	private static final int DEFAULT_STRINGBUILDER_CAPACITY = new StringBuilder().capacity();
 	private static final ConstantDeconstructor TOSTRING_DECONSTRUCTOR;
 	private static final ConstantDeconstructor NOARG_DECONSTRUCTOR = ConstructorBasedDeconstructor
 			.create(StringBuilder.class);
+	private static final ConstantDeconstructor CAPACITY_DECONSTRUCTOR;
 	static {
 		try {
 			TOSTRING_DECONSTRUCTOR = ConstructorBasedDeconstructor.create(StringBuilder.class,
 					DeconstructionDataAccessor.createForMethod(StringBuilder.class, "toString"));
+			CAPACITY_DECONSTRUCTOR = ConstructorBasedDeconstructor.create(StringBuilder.class,
+					DeconstructionDataAccessor.createForMethod(StringBuilder.class, "capacity"));
 		} catch (NoSuchMethodException e) {
 			throw new AssertionError(e);
 		}
@@ -23,6 +27,11 @@ final class StringBuilderConstantDeconstructor implements ConstantDeconstructor 
 			MethodNode methodnode, Object value) {
 		StringBuilder sb = (StringBuilder) value;
 		if (sb.length() == 0) {
+			//to check if the stringbuilder was constructed with a capacity constructor
+			int capacity = sb.capacity();
+			if (capacity != DEFAULT_STRINGBUILDER_CAPACITY) {
+				return CAPACITY_DECONSTRUCTOR.deconstructValue(context, transclass, methodnode, value);
+			}
 			return NOARG_DECONSTRUCTOR.deconstructValue(context, transclass, methodnode, value);
 		}
 		return TOSTRING_DECONSTRUCTOR.deconstructValue(context, transclass, methodnode, value);
