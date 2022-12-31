a172d7bf07fb245e7c75a6e5dd881076c6974018
sipkab <10866741+Sipkab@users.noreply.github.com>
2022-12-31 12:11:04 +0100

    Test field deconstructor in unrelated class

--- test/src/testing/sipka/jvm/constexpr/cli/FieldDeconstructorCliTest.java
+++ test/src/testing/sipka/jvm/constexpr/cli/FieldDeconstructorCliTest.java
@@ -18,7 +18,7 @@ public class FieldDeconstructorCliTest extends CliTestCase {
 	protected void runTestImpl(Map<String, String> parameters) throws Throwable {
 		Path injarpath = testCaseDirectory.resolve("classes.jar");
 		Path outjarpath = testCaseDirectory.resolve("out.jar");
-		TestUtils.writeJar(injarpath, Constants.class, CliSimpleConstantType.class);
+		TestUtils.writeJar(injarpath, Constants.class, CliSimpleConstantType.class, CacheFields.class);
 
 		Files.deleteIfExists(outjarpath);
 
@@ -30,22 +30,37 @@ public class FieldDeconstructorCliTest extends CliTestCase {
 		assertNonNull(TestUtils.getClInitMethod(classnode), "clinit method");
 
 		ClassLoader outcl = TestUtils.createJarClassLoader(outjarpath);
-		Object INIT_ZERO_value = Class.forName(Constants.class.getName(), false, outcl).getField("INIT_ZERO").get(null);
-		Object ZERO_value = Class.forName(CliSimpleConstantType.class.getName(), false, outcl).getField("ZERO")
-				.get(null);
+		Class<?> asmcontstantsclass = Class.forName(Constants.class.getName(), false, outcl);
+		Class<?> asmconstanttypeclass = Class.forName(CliSimpleConstantType.class.getName(), false, outcl);
+
+		Object INIT_ZERO = asmcontstantsclass.getField("INIT_ZERO").get(null);
+		Object ZERO = asmconstanttypeclass.getField("ZERO").get(null);
 		//should identity equal
-		assertIdentityEquals(INIT_ZERO_value, ZERO_value, "zero instance equality");
-		assertEquals(INIT_ZERO_value.getClass().getField("source").get(INIT_ZERO_value), "ZERO", "INIT_ZERO.source optimized");
+		assertIdentityEquals(INIT_ZERO, ZERO, "zero instance equality");
+		assertEquals(INIT_ZERO.getClass().getField("source").get(INIT_ZERO), "ZERO", "INIT_ZERO.source optimized");
 		assertEquals(Constants.INIT_ZERO.source, "constructor", "INIT_ZERO.source current");
 
-		Object INIT_ONE_value = Class.forName(Constants.class.getName(), false, outcl).getField("INIT_ONE").get(null);
-		assertEquals(INIT_ONE_value.getClass().getField("value").get(INIT_ONE_value), 1, "INIT_ONE.value");
+		Object INIT_ONE = asmcontstantsclass.getField("INIT_ONE").get(null);
+		assertEquals(INIT_ONE.getClass().getField("value").get(INIT_ONE), 1, "INIT_ONE.value");
+
+		Object INIT_HUNDRED = asmcontstantsclass.getField("INIT_HUNDRED").get(null);
+		assertEquals(asmconstanttypeclass.getField("value").get(INIT_HUNDRED), 100, "INIT_HUNDRED.value");
+		assertEquals(asmconstanttypeclass.getField("source").get(INIT_HUNDRED), "HUNDRED", "INIT_HUNDRED.source");
 	}
 
 	public static class Constants {
 
 		public static final CliSimpleConstantType INIT_ZERO = new CliSimpleConstantType(0);
 		public static final CliSimpleConstantType INIT_ONE = new CliSimpleConstantType(1);
+		public static final CliSimpleConstantType INIT_HUNDRED = new CliSimpleConstantType(100);
+	}
+
+	public static class CacheFields {
+		@Deconstructor
+		public static final CliSimpleConstantType HUNDRED = new CliSimpleConstantType(100);
+		static {
+			HUNDRED.source = "HUNDRED";
+		}
 	}
 
 	@ConstantExpression
