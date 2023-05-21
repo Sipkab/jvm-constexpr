bbbacba48dd4c3f33c8ba60e41895e000fed54a7
sipkab <10866741+Sipkab@users.noreply.github.com>
2023-05-21 17:21:08 +0200

    Fill out version in LicensesCommand

--- main/src/sipka/jvm/constexpr/main/LicensesCommand.java
+++ main/src/sipka/jvm/constexpr/main/LicensesCommand.java
@@ -1,6 +1,8 @@
 package sipka.jvm.constexpr.main;
 
 import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.jar.Manifest;
 
 import sipka.jvm.constexpr.tool.Utils;
 
@@ -10,19 +12,39 @@ import sipka.jvm.constexpr.tool.Utils;
  * </pre>
  */
 public class LicensesCommand {
+	// our own attribute name instead of deriving it from Nest-Bundle-Identifier
+	// because we don't want to deal with its format (even if its straightforward)
+	private static final String MANIFEST_VERSION_ATTRIBUTE_NAME = "Sipka-Jvm-Constexpr-Version";
+
 	public void call() throws Exception {
-		System.out.println("jvm-constexpr version " /*TODO set version*/);
-		System.out.println("Copyright (C) 2022 Bence Sipka");
-		System.out.println();
-		try (InputStream in = CliMain.class.getClassLoader().getResourceAsStream("META-INF/LICENSE")) {
-			Utils.copyStream(in, System.out);
+		ClassLoader classloader = CliMain.class.getClassLoader();
+		PrintStream out = System.out;
+
+		String version = "<unknown>";
+		try (InputStream in = classloader.getResourceAsStream("META-INF/MANIFEST.MF")) {
+			if (in != null) {
+				//just in case, the code might get bundled a different way
+				Manifest mf = new Manifest(in);
+				version = mf.getMainAttributes().getValue(MANIFEST_VERSION_ATTRIBUTE_NAME);
+			}
 		}
-		System.out.println();
-		System.out.println("----------");
-		System.out.println("Embedded software licenses");
-		System.out.println();
-		try (InputStream in = CliMain.class.getClassLoader().getResourceAsStream("META-INF/BUNDLED-LICENSES")) {
-			Utils.copyStream(in, System.out);
+
+		out.println("jvm-constexpr version " + version);
+		out.println("Copyright (C) 2023 Bence Sipka");
+		out.println();
+		try (InputStream in = classloader.getResourceAsStream("META-INF/LICENSE")) {
+			if (in != null) {
+				Utils.copyStream(in, out);
+			}
+		}
+		out.println();
+		out.println("----------");
+		out.println("Embedded software licenses");
+		out.println();
+		try (InputStream in = classloader.getResourceAsStream("META-INF/BUNDLED-LICENSES")) {
+			if (in != null) {
+				Utils.copyStream(in, out);
+			}
 		}
 	}
 }
